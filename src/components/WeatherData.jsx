25beed18a338d5ea0bb8bb8773d39e55be4f248e
BerinMadzak <berinmadzak@gmail.com>
2024-12-23 15:40:28 +0100

    Display weather data for location

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,11 +1,30 @@
 export default function WeatherData({data}) {
     if(!data) return <p>Enter a location to display weather data!</p>;
+
+    let date = new Date(data.date);
+    console.log(date);
     return (
         <div>
-            <h1>{data.address}</h1>
+            <h1 className="location">{data.address}</h1>
+            <div className="general-display">
+                <p>{data.conditions}</p>
+                <p>{date.toDateString()}</p>
+            </div>
+            <div className="weather-data">
+                <div className="weather-main">
+                    <img src={`icons/${data.icon}.svg`} alt={data.icon} />
+                    <div className="temp-display">
+                        <p className="temp">{data.temp + " °C"}</p>
+                        <p className="feels-like">Feels like: {data.feelsLike} °C</p>
+                    </div>
+                </div>
+                <div className="other-display">
+                    <p>Precipitation: {data.precipProb}%</p>
+                    <p>Humidity: {data.humidity}%</p>
+                    <p>Wind: {data.windSpeed} km/h</p>
+                </div>
+            </div>
             <p>{data.description}</p>
-            <img src={`icons/${data.icon}.png`} alt={data.icon} />
-            <p>{data.temp + " °C"}</p>
         </div>
     );
 }
--- src/helpers/weatherData.js
+++ src/helpers/weatherData.js
@@ -1,7 +1,7 @@
 export async function getWeatherData(location) {
     if(location === '') return;
     try {
-        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next6days?key=ARBPLGYL42FBD25ZVRQWC289P&contentType=json`);
+        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next6days?unitGroup=metric&key=ARBPLGYL42FBD25ZVRQWC289P&contentType=json`);
         let data = await response.json();
         console.log(data);
         return processData(data);
@@ -15,13 +15,13 @@ function processData(data) {
     return {
         address: data.resolvedAddress,
         description: data.description,
-        time: data.currentConditions.datetime,
+        date: data.days[0].datetime,
         conditions: data.currentConditions.conditions,
         humidity: data.currentConditions.humidity,
         temp: data.currentConditions.temp,
         feelsLike: data.currentConditions.feelslike,
         windSpeed: data.currentConditions.windspeed,
-        windDir: data.currentConditions.winddir,
+        precipProb: data.currentConditions.precipprob,
         icon: data.currentConditions.icon,
     };
 }
