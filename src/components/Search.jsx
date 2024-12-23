b979de7013e0b43d17735c3dd9c56db1bba6e324
BerinMadzak <berinmadzak@gmail.com>
2024-12-23 16:59:44 +0100

    Improve styling for search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ export default function Search({loadData}) {
     return (
         <form onSubmit={loadData}>
             <label htmlFor="location">Location: </label>
-            <input type="text" id="location" name="location"/>
+            <input type="text" id="location" name="location" autoComplete="off"/>
             <button type="submit">Search</button>
         </form>
     );
